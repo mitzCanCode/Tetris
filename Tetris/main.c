1bd0eb7535bff698b185905471f1102b79cb1616
Dimitris Chatzigeorgiou <dimitris.chatzi@proton.me>
2025-11-27 10:53:13 +0200

    Initial Commit

---
+++ Tetris/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Tetris
+//
+//  Created by mitz on 27/11/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
